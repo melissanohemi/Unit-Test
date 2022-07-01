ff6d5005a876df6730678242fdf67e9975020ba5
melissanohemi <melissa.castro1118@gmail.com>
2022-07-01 00:40:59 +0000

    unit test complete

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("Hello World")
+// we declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro){
+    // convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.2;
+    //return the dollar value
+    return valueInDollar;
+    }
+    const fromDollarToYen = function(valueInDollar){
+    let valueInYen = (valueInDollar / 1.2 ) * 127.9;
+    return valueInYen;
+    
+    }
+    
+    // this is my function that sums two numbers
+    const sum = (a,b) => {
+    return a + b
+    }
+    
+    
+    const fromYenToPound = function (valueInYen){
+        let valueInPound = (valueInYen/127.9) * .8
+        return valueInPound
+    }
+    module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
+    
+   
